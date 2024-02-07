e7e739f0571c23e1e29094e4bf15bcd4652a5b43
Harshit_Dahiya <dahiya.harshit12@gmail.com>
2024-02-08 00:48:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+function App() {
+  
+
+  return (
+    <main>
+    <div id="game-container">
+      <ol id="players">
+        <li >
+          <span className="player-name">Player 1</span>
+          <span className="player-symbol">X</span>
+        </li> 
+        <li>
+        <span className="player-name">Player 2</span>
+          <span className="player-symbol">O</span>
+        </li>
+      </ol>
+      GAME BOARD
+    </div>
+  </main>
+  )
+}
+
+export default App
